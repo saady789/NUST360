be96b79c046f47524111c8c84db2f95a53c16425
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-12-01 00:39:17 +0500

    front page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,19 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      "lightblue": "#dcebf3",
+      "darkblue": "#34445b",
+      "reed": "#eb3b3c",
+      "sdarkblue": "#728192",
+      "sred": "#de838c",
+      "slightblue": " #b1c0cf",
+      "qbg":"#e7dbdb",
+      "qblue":"#0262b4",
+      "white":"#f9f9f9"
+
+
+    }
   },
   plugins: [],
 }
